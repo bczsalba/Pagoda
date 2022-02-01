c893af27280e4fbc960d5520884b6ad28330ba15
bczsalba <bczsalba@gmail.com>
2022-02-01 10:52:38 +0100

    Update `PagodaApplication` protocol

--- pagoda/applications/__init__.py
+++ pagoda/applications/__init__.py
@@ -1,4 +1,4 @@
-"""The module containing special windows for Pagoda."""
+"""The module containing the builtin applications for Pagoda."""
 
 from .application import PagodaApplication
 from .teahaz import TeahazApplication
--- pagoda/applications/application.py
+++ pagoda/applications/application.py
@@ -1,8 +1,11 @@
 """The module containing the PagodaApplication parent class."""
 
+from typing import Any
 from abc import ABC, abstractmethod
 
 import pytermgui as ptg
+
+from ..widgets import Header
 
 
 class PagodaApplication(ABC):
@@ -25,9 +28,42 @@ class PagodaApplication(ABC):
         self.manager = manager
         self.active_windows = []
 
+    def _get_base_window(self, **attrs: Any) -> ptg.Window:
+        """Constructs a base window for this application.
+
+        For the time being, all this does is add a Header with self.title.
+
+        Args:
+            **attrs: Arbitrary attributes passed to the window.
+        """
+
+        window = ptg.Window(**attrs)
+        window += Header("[title]" + self.title)
+        return window
+
     @abstractmethod
-    def construct_window(self) -> ptg.Window:
-        """Constructs and returns a window."""
+    def construct_window(self, **attrs: Any) -> ptg.Window:
+        """Constructs and returns a window.
+
+        Args:
+            **attrs: Arbitrary attributes passed to the new window.
+
+        Returns:
+            An application window.
+        """
+
+    def close(self, window: ptg.Window) -> None:
+        """Closes given window, while also removing it from our active_windows.
+
+        Args:
+            window: The window to close.
+        """
+
+        if not window in self.active_windows:
+            raise ValueError(f'Window "{window}" is not in {self}\'s active_windows.')
+
+        self.active_windows.remove(window)
+        window.close()
 
     def stop(self) -> None:
         """Closes all application windows.
